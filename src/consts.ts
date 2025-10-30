f6d70a1237fe6b3741cc46e7e03ed27ca96d0810
Yibin Long <yibinlong@outlook.com>
2025-10-30 12:55:41 -0400

    feat: customize site metadata
    - Update site title to 'Yibin Long'
    
    - Update site description to match personal brand

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Yibin Long';
+export const SITE_DESCRIPTION = 'Notes on software, product, and occasional life.';
